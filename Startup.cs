63268b1fad6374c096598dee7fd2d4d4ba177c92
Christin Jemildan Anton Yogarajah <jemildan@gmail.com>
2019-12-06 11:44:47 +0000

    added refereces to the project

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using Taste.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Taste.DataAccess;
 
 namespace Taste
 {
