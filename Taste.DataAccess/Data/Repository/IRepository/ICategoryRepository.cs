0a7ffa970e061e32a9c673ab20b54e58d08a40a0
Christin Jemildan Anton Yogarajah <jemildan@gmail.com>
2019-12-06 13:32:40 +0000

    implemented ICategoryRepository

--- Taste.DataAccess/Data/Repository/IRepository/ICategoryRepository.cs
+++ Taste.DataAccess/Data/Repository/IRepository/ICategoryRepository.cs
@@ -1,7 +1,13 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Taste.Models;
+
 namespace Taste.DataAccess.Data.Repository.IRepository
 {
-    public interface ICategoryRepository
+    public interface ICategoryRepository : IRepository<Category>
     {
-         
+        IEnumerable<SelectListItem> GetCategoryListForDropDown();
+
+        void Update(Category category);
     }
 }
