fc2a336a20df983621d616354a40b415ec6c75ba
Christin Jemildan Anton Yogarajah <jemildan@gmail.com>
2019-12-06 13:14:45 +0000

    implemented IRepository

--- Taste.DataAccess/Data/Repository/IRepository/IRepository.cs
+++ Taste.DataAccess/Data/Repository/IRepository/IRepository.cs
@@ -1,7 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
 namespace Taste.DataAccess.Data.Repository.IRepository
 {
-    public interface IRepository
+    public interface IRepository<T> where T : class
     {
-         
+        /* ----Basic Repository to do CRUD operations -----*/
+
+        // Get a particular element
+        T Get(int id);
+
+        // Get all elements with specified query
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null
+            );
+
+        // Get first or default element
+        T GetFirstOrDefault(
+            Expression<Func<T, bool>> filter = null,
+            string includeProperties = null
+            );
+
+        // add an element
+        void Add(T entity);
+
+        // Remove the specified element
+        void Remove(int id);
+
+        // Remove the complete entity
+        void Remove(T entity);
     }
 }
