cf5527324b5df655581f5f1b284a2a4d22ab9e25
Christin Jemildan Anton Yogarajah <jemildan@gmail.com>
2019-12-06 19:06:29 +0000

    created FoodType data model, IFoodTypeRepository and FoodTypeRepository

---
+++ Taste.DataAccess/Data/Repository/IRepository/IFoodTypeRepository.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Taste.Models;
+
+namespace Taste.DataAccess.Data.Repository.IRepository
+{
+    public interface IFoodTypeRepository : IRepository<FoodType>
+    {
+        IEnumerable<SelectListItem> GetFoodTypeListForDropDown();
+
+        void Update(FoodType foodType);
+    }
+}
