01145f7cf1ede1d6444100a7599450f09d3b306b
Christin Jemildan Anton Yogarajah <jemildan@gmail.com>
2019-12-07 18:25:08 +0000

    Added Items To Shopping Cart

--- Taste.DataAccess/Data/Repository/UnitOfWork.cs
+++ Taste.DataAccess/Data/Repository/UnitOfWork.cs
@@ -13,6 +13,7 @@ namespace Taste.DataAccess.Data.Repository
             FoodType = new FoodTypeRepository(_db);
             MenuItem = new MenuItemRepository(_db);
             ApplicationUser = new ApplicationUserRepository(_db);
+            ShoppingCart = new ShoppingCartRepository(_db);
         }
 
         public ICategoryRepository Category { get; private set; }
@@ -20,6 +21,7 @@ namespace Taste.DataAccess.Data.Repository
         public IFoodTypeRepository FoodType { get; private set; }
         public IMenuItemRepository MenuItem { get; private set; }
         public IApplicationUserRepository ApplicationUser { get; private set; }
+        public IShoppingCartRepository ShoppingCart { get; private set; }
         public void Dispose()
         {
             _db.Dispose();
