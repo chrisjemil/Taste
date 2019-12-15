24fa15a1b14598a01e7207a2079623a2d275eb86
Christin Jemildan Anton Yogarajah <jemildan@gmail.com>
2019-12-15 12:55:54 +0000

    added refund order

--- Taste/Pages/Admin/Order/ManageOrder.cshtml.cs
+++ Taste/Pages/Admin/Order/ManageOrder.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Stripe;
 using Taste.DataAccess.Data.Repository.IRepository;
 using Taste.Models;
 using Taste.Models.ViewModels;
@@ -73,7 +74,14 @@ namespace Taste.Pages.Admin.Order
         {
             OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(o => o.Id == orderId);
             //refund amoount
-
+            var options = new RefundCreateOptions
+            {
+                Amount = Convert.ToInt32(orderHeader.OrderTotal*100),
+                Reason = RefundReasons.RequestedByCustomer,
+                Charge = orderHeader.TransactionId
+            };
+            var service = new RefundService();
+            Refund refund = service.Create(options);
             orderHeader.Status = SD.StatusRefunded;
             _unitOfWork.Save();
             return RedirectToPage("ManageOrder");
